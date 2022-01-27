9dc3371868c61f70e7fdf5673674276c1fece6ad
AntarktisZ11 <marcus.borne@outlook.com>
2022-01-27 03:40:10 +0100

    + Adjustable deep-sleep time

--- src/app_httpd.cpp
+++ src/app_httpd.cpp
@@ -243,6 +243,7 @@ static esp_err_t cmd_handler(httpd_req_t *req)
         0,
     };
 
+    int res = 0;
     buf_len = httpd_req_get_url_query_len(req) + 1;
     if (buf_len > 1)
     {
@@ -258,28 +259,86 @@ static esp_err_t cmd_handler(httpd_req_t *req)
                 httpd_query_key_value(buf, "val", value, sizeof(value)) == ESP_OK)
             {
                 // Enters if query has 'key=var' structure
+
+                int val = atoi(value);
+                sensor_t *s = esp_camera_sensor_get();
+
+                if (!strcmp(variable, "sleep_time"))
+                {
+                    if (!setSleepTime(val))
+                    {
+                        free(buf);
+                        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Too large sleep time requested");
+                        return ESP_FAIL;
+                    }
+                }
+                else if (!strcmp(variable, "framesize"))
+                {
+                    if (s->pixformat == PIXFORMAT_JPEG)
+                        res = s->set_framesize(s, (framesize_t)val);
+                }
+                else if (!strcmp(variable, "quality"))
+                    res = s->set_quality(s, val);
+                else if (!strcmp(variable, "contrast"))
+                    res = s->set_contrast(s, val);
+                else if (!strcmp(variable, "brightness"))
+                    res = s->set_brightness(s, val);
+                else if (!strcmp(variable, "saturation"))
+                    res = s->set_saturation(s, val);
+                else if (!strcmp(variable, "gainceiling"))
+                    res = s->set_gainceiling(s, (gainceiling_t)val);
+                else if (!strcmp(variable, "colorbar"))
+                    res = s->set_colorbar(s, val);
+                else if (!strcmp(variable, "awb"))
+                    res = s->set_whitebal(s, val);
+                else if (!strcmp(variable, "agc"))
+                    res = s->set_gain_ctrl(s, val);
+                else if (!strcmp(variable, "aec"))
+                    res = s->set_exposure_ctrl(s, val);
+                else if (!strcmp(variable, "hmirror"))
+                    res = s->set_hmirror(s, val);
+                else if (!strcmp(variable, "vflip"))
+                    res = s->set_vflip(s, val);
+                else if (!strcmp(variable, "awb_gain"))
+                    res = s->set_awb_gain(s, val);
+                else if (!strcmp(variable, "agc_gain"))
+                    res = s->set_agc_gain(s, val);
+                else if (!strcmp(variable, "aec_value"))
+                    res = s->set_aec_value(s, val);
+                else if (!strcmp(variable, "aec2"))
+                    res = s->set_aec2(s, val);
+                else if (!strcmp(variable, "dcw"))
+                    res = s->set_dcw(s, val);
+                else if (!strcmp(variable, "bpc"))
+                    res = s->set_bpc(s, val);
+                else if (!strcmp(variable, "wpc"))
+                    res = s->set_wpc(s, val);
+                else if (!strcmp(variable, "raw_gma"))
+                    res = s->set_raw_gma(s, val);
+                else if (!strcmp(variable, "lenc"))
+                    res = s->set_lenc(s, val);
+                else if (!strcmp(variable, "special_effect"))
+                    res = s->set_special_effect(s, val);
+                else if (!strcmp(variable, "wb_mode"))
+                    res = s->set_wb_mode(s, val);
+                else if (!strcmp(variable, "ae_level"))
+                    res = s->set_ae_level(s, val);
+                else
+                {
+                    res = -1;
+                }
             }
             else if (!strcmp(buf, "led_toggle"))
-            {
                 ledBuiltIn.toggle();
-            }
             else if (!strcmp(buf, "led_on"))
-            {
                 ledBuiltIn.on();
-            }
             else if (!strcmp(buf, "led_off"))
-            {
                 ledBuiltIn.off();
-            }
             else if (!strcmp(buf, "flash_toggle"))
-            {
                 flashlight.toggle();
-            }
             else if (!strcmp(buf, "restart"))
-            {
                 ESP.restart(); // tells the SDK to reboot vs 'ESP.reset()' which is a hard reset and can
                                // leave some of the registers in the old state which can lead to problems.
-            }
             else if (!strcmp(buf, "stay_awake"))
             {
                 setStayAwakeTime(120);
@@ -311,66 +370,6 @@ static esp_err_t cmd_handler(httpd_req_t *req)
     {
         httpd_resp_send_404(req);
         return ESP_FAIL;
-    }
-
-    int val = atoi(value);
-    sensor_t *s = esp_camera_sensor_get();
-    int res = 0;
-
-    if (!strcmp(variable, "framesize"))
-    {
-        if (s->pixformat == PIXFORMAT_JPEG)
-            res = s->set_framesize(s, (framesize_t)val);
-    }
-    else if (!strcmp(variable, "quality"))
-        res = s->set_quality(s, val);
-    else if (!strcmp(variable, "contrast"))
-        res = s->set_contrast(s, val);
-    else if (!strcmp(variable, "brightness"))
-        res = s->set_brightness(s, val);
-    else if (!strcmp(variable, "saturation"))
-        res = s->set_saturation(s, val);
-    else if (!strcmp(variable, "gainceiling"))
-        res = s->set_gainceiling(s, (gainceiling_t)val);
-    else if (!strcmp(variable, "colorbar"))
-        res = s->set_colorbar(s, val);
-    else if (!strcmp(variable, "awb"))
-        res = s->set_whitebal(s, val);
-    else if (!strcmp(variable, "agc"))
-        res = s->set_gain_ctrl(s, val);
-    else if (!strcmp(variable, "aec"))
-        res = s->set_exposure_ctrl(s, val);
-    else if (!strcmp(variable, "hmirror"))
-        res = s->set_hmirror(s, val);
-    else if (!strcmp(variable, "vflip"))
-        res = s->set_vflip(s, val);
-    else if (!strcmp(variable, "awb_gain"))
-        res = s->set_awb_gain(s, val);
-    else if (!strcmp(variable, "agc_gain"))
-        res = s->set_agc_gain(s, val);
-    else if (!strcmp(variable, "aec_value"))
-        res = s->set_aec_value(s, val);
-    else if (!strcmp(variable, "aec2"))
-        res = s->set_aec2(s, val);
-    else if (!strcmp(variable, "dcw"))
-        res = s->set_dcw(s, val);
-    else if (!strcmp(variable, "bpc"))
-        res = s->set_bpc(s, val);
-    else if (!strcmp(variable, "wpc"))
-        res = s->set_wpc(s, val);
-    else if (!strcmp(variable, "raw_gma"))
-        res = s->set_raw_gma(s, val);
-    else if (!strcmp(variable, "lenc"))
-        res = s->set_lenc(s, val);
-    else if (!strcmp(variable, "special_effect"))
-        res = s->set_special_effect(s, val);
-    else if (!strcmp(variable, "wb_mode"))
-        res = s->set_wb_mode(s, val);
-    else if (!strcmp(variable, "ae_level"))
-        res = s->set_ae_level(s, val);
-    else
-    {
-        res = -1;
     }
 
     if (res)
@@ -416,7 +415,8 @@ static esp_err_t status_handler(httpd_req_t *req)
     p += sprintf(p, "\"dcw\":%u,", s->status.dcw);
     p += sprintf(p, "\"colorbar\":%u,", s->status.colorbar);
     p += sprintf(p, "\"led_on\":%u,", ledBuiltIn.status());
-    p += sprintf(p, "\"stay_awake_time\":%u", getStayAwakeTime());
+    p += sprintf(p, "\"stay_awake_time\":%u,", getStayAwakeTime());
+    p += sprintf(p, "\"sleep_time\":%u", getSleepTime());
     *p++ = '}';
     *p++ = 0; // NULL byte to signify EOL
 
